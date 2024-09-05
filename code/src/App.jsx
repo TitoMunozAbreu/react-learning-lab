24626a9e9047247d5a8df4ffa8ab454a2d012901
TitoMunozAbreu <97183342+TitoMunozAbreu@users.noreply.github.com>
2024-09-05 17:41:01 +0200

    finished essentianls

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -7,11 +7,24 @@ import { EXAMPLES } from "./data";
 
 function App() {
   const tabTitles = ["components", "jsx", "props", "state"];
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(btnSelected) {
     setSelectedTopic(btnSelected);
-    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select a topic!</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -21,31 +34,25 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
             {tabTitles.map((tabTitle) => (
-              <TabButton onSelect={() => handleSelect(tabTitle)} key={tabTitle}>
+              <TabButton
+                isSelected={selectedTopic === tabTitle}
+                onSelect={() => handleSelect(tabTitle)}
+                key={tabTitle}
+              >
                 {tabTitle}
               </TabButton>
             ))}
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
